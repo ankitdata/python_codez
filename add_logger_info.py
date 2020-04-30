594c2bdb48b675e7cf4af63de03cd5e49ff0d4e2
Ankit Chilkalwar <50068297+ankitdata@users.noreply.github.com>
2020-04-30 17:08:20 +0400

    add how to create log file

---
+++ add_logger_info.py
@@ -0,0 +1,22 @@
+"""
+This will help to create log file and save info and errors
+"""
+import logging
+import logging.handlers
+_logger = logging.getLogger(__name__)
+_logger.addHandler(logging.StreamHandler(sys.stdout))
+_logger.setLevel(logging.INFO)
+
+filename = 'your_file.log'
+
+my_logger = logging.getLogger(msg_id)
+my_logger.setLevel(logging.INFO)
+handler = logging.handlers.RotatingFileHandler(filename, maxBytes=20)
+my_logger.addHandler(handler)
+
+my_logger.info('add message here')
+
+try :
+    #add something here
+except BaseException as error:
+            my_logger.error('{}'.format(error))
